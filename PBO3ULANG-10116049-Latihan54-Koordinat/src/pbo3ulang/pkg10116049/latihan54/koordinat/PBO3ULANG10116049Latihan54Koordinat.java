e252d4a1ebd61b274b55db5830dd3171eb89e03c
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-02-19 20:52:13 +0700

    Add files via upload

---
+++ PBO3ULANG-10116049-Latihan54-Koordinat/src/pbo3ulang/pkg10116049/latihan54/koordinat/PBO3ULANG10116049Latihan54Koordinat.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan54.koordinat;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA              : M. Ibad Guthwara
+ * KELAS             : PBO 3 ULANG
+ * NIM               : 10116049
+ * DESKRIPSI PROGRAM :
+ */
+public class PBO3ULANG10116049Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKoordinat warnaKoordinat = new WarnaKoordinat(10,4,"Jingga");
+        System.out.println("Warna Koordinat : "+warnaKoordinat.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : "+warnaKoordinat.getX()+", y : "+warnaKoordinat.getY());
+    }
+    
+}
